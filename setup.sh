dad616a7e37ac7918bc9ccd6a2523a7e9445f749
Chad Lavimoniere <chad@chadlavimoniere.com>
2023-10-15 00:31:42 +0000

    first draft

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# install homebrew
+xcode-select --install && \
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
+# install a few essential programs with homebrew
+brew install git vim nano tmux w3m
+
+# set vimrc
+echo "$(curl -fsSL https://gitlab.com/chadlavimoniere/quick-setup/-/raw/main/vimrc)" > ~/.nanorc
+
+# set nanorc
+echo "$(curl -fsSL https://gitlab.com/chadlavimoniere/quick-setup/-/raw/main/nanorc)" > ~/.nanorc
+
+# install ohmyzsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
---
+++ vimrc
@@ -0,0 +1,8 @@
+set nocompatible
+colo torte
+syntax on
+set tabstop =2
+set softtabstop =2
+set shiftwidth =2
+set expandtab
+set smartindent
