dc0f56415c507c2ea83fe4f916de7a86926303a8
Chad Lavimoniere <chad@chadlavimoniere.com>
2023-10-15 00:34:47 +0000

    update install order and readme

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# quick-setup
+# Quick zsh setup for new mac
 
 A one-liner quick setup script for some of my personal comfort preferences on a new mac
 
 ```sh
 sh -c "$(curl -fsSL https://gitlab.com/chadlavimoniere/quick-setup/-/raw/main/setup.sh)"
 ```
+
+## What does it do?
+
+1. installs `homebrew`
+    - uses homebrew to install `git` `vim` `nano` `tmux`, and `w3m`
+1. installs `ohmyzsh`
+1. sets a `.vimrc`
+1. sets a `.nanorc`
--- setup.sh
+++ setup.sh
@@ -6,12 +6,11 @@ xcode-select --install && \
 # install a few essential programs with homebrew
 brew install git vim nano tmux w3m
 
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # set vimrc
 echo "$(curl -fsSL https://gitlab.com/chadlavimoniere/quick-setup/-/raw/main/vimrc)" > ~/.nanorc
 
 # set nanorc
 echo "$(curl -fsSL https://gitlab.com/chadlavimoniere/quick-setup/-/raw/main/nanorc)" > ~/.nanorc
-
-# install ohmyzsh
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
